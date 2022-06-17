0858fe4af7b5565b66f0ad2d06e900a90c5df4bd
pandasoli <sadpandasoli@gmail.com>
2022-06-17 13:44:59 -0300

    [update] removed my m3o api key

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const config = {
   env: {
     mongoUri: '',
     authKey: '703caf74c67dc0dea2b18068431945d5', // PandaSoli-Default
-    m3oKey: 'ZTU1MTA5ZDQtODRjOS00NmM3LWEyZGQtMTA3YWU0YWVjZjI3'
+    m3oKey: ''
   }
 }
 
