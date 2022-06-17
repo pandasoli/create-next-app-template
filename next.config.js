1bfbb980d2e1b2373aa0a544685c826c6353ece3
pandasoli <sadpandasoli@gmail.com>
2022-06-17 03:07:27 -0300

    [move] move exports to the end

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+
+const config = {
   reactStrictMode: true,
   env: {
+    mongoUri: '',
     authKey: '703caf74c67dc0dea2b18068431945d5', // PandaSoli-Default
     m3oKey: 'ZTU1MTA5ZDQtODRjOS00NmM3LWEyZGQtMTA3YWU0YWVjZjI3'
   }
 }
+
+
+module.exports = config
